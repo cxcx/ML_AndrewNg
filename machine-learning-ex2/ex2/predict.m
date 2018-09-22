2b44becf5930637a452560dabece690d1f394833
jack <lplcx@live.com>
2018-09-22 23:54:10 +0800

    1.complete the prediction part

--- machine-learning-ex2/ex2/predict.m
+++ machine-learning-ex2/ex2/predict.m
@@ -15,13 +15,12 @@ p = zeros(m, 1);
 %               You should set p to a vector of 0's and 1's
 %
 
-
-
-
-
-
-
+for i = 1 : m
+    p_value = sigmoid(X(i, :) * theta);
+    if p_value >= 0.5
+        p(i) = 1;
+    else
+        p(i) = 0;
 % =========================================================================
-
-
+end
 end

557a7a5e42060987a3cdf226f22107b6c8e0bcfd
jack <lplcx@live.com>
2018-09-24 13:42:24 +0800

    1.临时提交 正则化代价函数

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -6,6 +6,7 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
 
 % Initialize some useful values
 m = length(y); % number of training examples
+n = length(theta);
 
 % You need to return the following variables correctly 
 J = 0;
@@ -17,11 +18,40 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+s = 0;
+for i = 1 : m
+    s = s + (-1 * y(i) * log(h_theta_function(X(i, :), theta)) ...
+        - (1 - y(i)) * log(1 - h_theta_function(X(i, :), theta)));
+end
+% caculate the sum of regularized part
+s_theta = 0;
+for j = 2 : n
+    s_theta = s_theta + (theta(j) * theta(j));
+end
 
+J = s / m + lambda * s_theta / (2 * m);
 
-
-
-
+% ====caculate gradiend vector====
+for i = 1 : n
+    s = 0;
+    for j = 1 : m
+        s = s + (h_theta_function(X(j, :), theta) - y(i)) * X(j, i);
+    end
+    if i > 1
+        s = s + lambda * theta(i);
+    end
+    grad(i) = s / m;
+end
 % =============================================================
 
+end
+
+% h_theta_function
+% h(x) = g(X * theta);
+% g(z) = 1 / (1 + exp(-z));
+function h = h_theta_function(x, theta)
+% x caculate h(x) = g(Xt * theta), g is sigmoid function
+
+h = 0;
+h = sigmoid(x * theta);
 end
